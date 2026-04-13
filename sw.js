e75ad62ae1256f5b0981d36249fddcbeacc90d9e
Barakuda989 <magnum.gopnik@gmail.com>
2026-04-13 21:45:07 +0300

    cd ~/Desktop/kreptonit-calc-git

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker — Крептонит Калькуляторы
 // Кэширует все файлы для работы офлайн на Android
 
-const CACHE_NAME = 'kreptonit-v19';
+const CACHE_NAME = 'kreptonit-v20';
 
 // Файлы для кэширования при установке
 const FILES_TO_CACHE = [
